a7d485a89ecead5d8770f38724e4bda15387b2a2
Paulo H. Rodrigues <qualvalordex@gmail.com>
2021-02-27 00:06:33 -0300

    Comments at example file

--- extractors/Tweext/example.py
+++ extractors/Tweext/example.py
@@ -1,10 +1,18 @@
 from Tweext import Tweext
 import time
 
+# Initializing time counting
 init = time.time()
-tweext = Tweext()
-tweext.authenticate()
-tweext.start_extraction(keyword = 'vacina')
-end = time.time()
 
+# Initializing the class
+tweext = Tweext()
+
+# Authenticating...
+tweext.authenticate()
+
+# Downloading tweets related to the specific keyword
+tweext.start_extraction(keyword = 'vacina')
+
+# Time counting finished
+end = time.time()
 print('Executed in {}'.format(end-init))
