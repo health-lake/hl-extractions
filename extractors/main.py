8c35b138369b4256e667fce4a851431a3710c89a
Rennan Haro <rharo@questrade.com>
2021-04-28 18:46:06 -0300

    style: linting w/ black formatter

--- extractors/main.py
+++ extractors/main.py
@@ -2,18 +2,17 @@ from extr_cnac import ExtractCNAC
 from extr_rciv import ExtractRCIV
 from extr_srag import ExtractSRAG
 
+
 def main():
     cnac = ExtractCNAC()
     cnac.download()
 
     # todo: where is this date() defined? is crashing the script whenever run using docker.
-    rciv = ExtractRCIV(
-        initial_date = date(2015,1,1),
-        final_dalte = date(2021,2,28)
-    )
+    rciv = ExtractRCIV(initial_date=date(2015, 1, 1), final_dalte=date(2021, 2, 28))
     rciv.download()
 
     srag = ExtractSRAG()
     srag.download()
+
 
 main()
