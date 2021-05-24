32fd9dabfec1852e66a6c6f9f43c00a5e2ac49d9
Rennan Haro <rennanharo@gmail.com>
2021-05-23 23:21:08 -0300

    hotfix: adding cli args option to datasource

--- extractors/main.py
+++ extractors/main.py
@@ -4,18 +4,22 @@ from extr_srag import ExtractSRAG
 
 from datetime import datetime
 
-
-def main():
-    cnac = ExtractCNAC()
-    cnac.download()
-
-    rciv = ExtractRCIV(
-        initial_date=datetime(2021, 1, 1), final_dalte=datetime(2021, 2, 28)
-    )
-    rciv.download()
-
-    srag = ExtractSRAG()
-    srag.download()
+import sys, getopt
 
 
-main()
+def main(source):
+    if source == "CNAC":
+        cnac = ExtractCNAC()
+        cnac.download()
+    elif source == "RCIV":
+        rciv = ExtractRCIV(
+            initial_date=datetime(2021, 1, 1), final_dalte=datetime(2021, 2, 28)
+        )
+        rciv.download()
+    elif source == "SRAG":
+        srag = ExtractSRAG()
+        srag.download()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1].upper())
