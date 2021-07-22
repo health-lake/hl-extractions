d019e2776414f71c1f41cbf2cabcf746e3e89ccb
Marcelo <marcelofelippe.mfg@gmail.com>
2021-07-21 21:12:45 -0300

    Mudando o boto 3 resource para client

--- utils/s3_writer_operator.py
+++ utils/s3_writer_operator.py
@@ -39,7 +39,7 @@ class S3WriterOperator:
         AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
         AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-        self.s3 =boto3.resource(
+        self.s3 =boto3.client(
             service_name="s3",
             region_name="us-east-1",
             aws_access_key_id=AWS_ACCESS_KEY_ID,
