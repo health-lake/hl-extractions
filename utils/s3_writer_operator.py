c93d21d58fd960698afa3acdf2be0477bdbaa9bd
Marcelo <marcelofelippe.mfg@gmail.com>
2021-07-21 21:04:15 -0300

    Corrigindo nome do parametro do boto3resource

--- utils/s3_writer_operator.py
+++ utils/s3_writer_operator.py
@@ -42,7 +42,7 @@ class S3WriterOperator:
         self.s3 =boto3.resource(
             service_name="s3",
             region_name="us-east-1",
-            aws_access_key=AWS_ACCESS_KEY_ID,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY
         )
 
