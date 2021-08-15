5c001c9c1a211ba24fb256d3e4d15c01d24a958f
Marcelo Guimaraes <marcelo.felippe@datasprints.com>
2021-08-14 22:22:39 -0300

    Adicionando extraçao para dados de vacina

--- modules/extr_vacina.py
+++ modules/extr_vacina.py
@@ -24,29 +24,6 @@ class ExtractVACINA:
         s3.upload_file(local_file, bucket, s3_file)
 
 
-    ############### Functions ###############
-    def upload_file_s3(self, file_name, bucket, object_name=None):
-        """Upload a file to an S3 bucket
-
-        :param file_name: File to upload
-        :param bucket: Bucket to upload to
-        :param object_name: S3 object name. If not specified then file_name is used
-        :return: True if file was uploaded, else False
-        """
-
-        # If S3 object_name was not specified, use file_name
-        if object_name is None:
-            object_name = file_name
-
-        # Upload the file
-        s3_client = boto3.client('s3')
-        try:
-            response = s3_client.upload_file(file_name, bucket, object_name)
-        except ClientError as e:
-            print(e)
-            return False
-        return True
-
     ############### MAIN CODE ###############
     # # Primeira request
     ## FULLLOAD
